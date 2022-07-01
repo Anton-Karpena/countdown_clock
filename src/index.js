b303049764ed569f4b79bc3c6abc9d8574b64f11
Anton Karpena <antoniokarpena@gmail.com>
2022-07-01 13:07:47 +0300

    mobile and tablet layouts changes

--- src/index.js
+++ src/index.js
@@ -2,16 +2,6 @@ const calendar = document.getElementById('date');
 const button = document.getElementById('button');
 let countDate = null;
 
-calendar.addEventListener('input', (event) => {
-  const result = new Date(event.target.value).getTime();
-  countDate = result;
-  countdown();
-})
-
-button.addEventListener('click', () => {
-  setInterval(countdown, 1000);
-})
-
 const countdown = () => {
   const now = new Date().getTime();
   const gap = countDate - now;
@@ -25,9 +15,26 @@ const countdown = () => {
   const textHour = Math.floor((gap % day) / hour);
   const textMinute = Math.floor((gap % hour) / minute);
   const textSecond = Math.floor((gap % minute) / second);
-  
+
   document.querySelector('.clock--days').innerText = textDay;
   document.querySelector('.clock--hours').innerText = textHour;
   document.querySelector('.clock--minutes').innerText = textMinute;
   document.querySelector('.clock--seconds').innerText = textSecond;
-}
+};
+
+calendar.addEventListener('input', (event) => {
+  const result = new Date(event.target.value).getTime();
+  const now = new Date().getTime();
+
+  if (result <= now) {
+    alert('Please, enter correct date')
+    return;
+  } else {
+    countDate = result;
+    countdown();
+  }
+});
+
+button.addEventListener('click', () => {
+  setInterval(countdown, 1000);
+});
